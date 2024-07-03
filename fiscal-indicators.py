6180e617d959a7735b1b0de5d931dd91e6713db5
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-03 10:23:11 +0530

    Update fiscal-indicators.py

--- fiscal-indicators.py
+++ fiscal-indicators.py
@@ -46,7 +46,7 @@ st.markdown(hide_st_style, unsafe_allow_html =True)
 def loadfile():
 	password = st.secrets["db_password"]
 	excel_content = io.BytesIO()
-	with open("spectrum_map_protected.xlsx", 'rb') as f:
+	with open("goi-fiscal-indicators.xlsx", 'rb') as f:
 		excel = msoffcrypto.OfficeFile(f)
 		excel.load_key(password)
 		excel.decrypt(excel_content)
