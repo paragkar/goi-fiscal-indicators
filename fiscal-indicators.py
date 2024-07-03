d254f411f291d87e7d7bf524a8c3ff05c0a2e71f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-03 12:51:11 +0530

    Update fiscal-indicators.py

--- fiscal-indicators.py
+++ fiscal-indicators.py
@@ -10,27 +10,27 @@ pd.set_option('display.max_columns', None)
 
 # Hide Streamlit style and buttons
 hide_st_style = '''
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-    '''
+	<style>
+	#MainMenu {visibility: hidden;}
+	footer {visibility: hidden;}
+	header {visibility: hidden;}
+	</style>
+	'''
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Load file function
 @st.cache_resource
 def loadfile():
-    password = st.secrets["db_password"]
-    excel_content = io.BytesIO()
-    with open("goi-fiscal-indicators.xlsx", 'rb') as f:
-        excel = msoffcrypto.OfficeFile(f)
-        excel.load_key(password)
-        excel.decrypt(excel_content)
+	password = st.secrets["db_password"]
+	excel_content = io.BytesIO()
+	with open("goi-fiscal-indicators.xlsx", 'rb') as f:
+		excel = msoffcrypto.OfficeFile(f)
+		excel.load_key(password)
+		excel.decrypt(excel_content)
 
-    # Loading data from excel file
-    df = pd.read_excel(excel_content, sheet_name="Sheet1")
-    return df
+	# Loading data from excel file
+	df = pd.read_excel(excel_content, sheet_name="Sheet1")
+	return df
 
 # Main Program Starts Here
 df = loadfile()
@@ -61,8 +61,8 @@ filtered_df = df[df['Metric'].isin(selected_metrics)]
 
 # Plotly animation setup
 fig = px.scatter(filtered_df, x="Value", y="Metric", animation_frame="Date_str", animation_group="Metric",
-                 color="Metric", range_x=[-filtered_df['Value'].abs().max() - 10, filtered_df['Value'].max() + 1],
-                 title="", size_max=20, text="Text")
+				 color="Metric", range_x=[-filtered_df['Value'].abs().max() - 10, filtered_df['Value'].max() + 1],
+				 title="", size_max=20, text="Text")
 
 # Customize text position to the right of the dots
 fig.update_traces(textposition='middle right')
@@ -79,95 +79,102 @@ fig.update_layout(showlegend=False)
 
 # Add metric names on the left side of the chart after the negative x-axis ends
 for metric in filtered_df['Metric'].unique():
-    fig.add_annotation(
-        x=-filtered_df['Value'].abs().max() - 8,
-        y=metric,
-        text=metric,
-        showarrow=False,
-        xanchor='left',
-        font=dict(size=12)
-    )
+	fig.add_annotation(
+		x=-filtered_df['Value'].abs().max() - 8,
+		y=metric,
+		text=metric,
+		showarrow=False,
+		xanchor='left',
+		font=dict(size=12)
+	)
 
 # Adjust the layout
 fig.update_layout(
-    xaxis_title="Value as Percentage of GDP",
-    yaxis_title="",
-    height=800,  # Adjust the height to make the plot more visible
-    margin=dict(l=40, r=40, t=100, b=40),  # Add margins to make the plot more readable
-    sliders=[{
-        'steps': [
-            {
-                'args': [
-                    [date_str],
-                    {
-                        'frame': {'duration': 300, 'redraw': True},
-                        'mode': 'immediate',
-                        'transition': {'duration': 300}
-                    }
-                ],
-                'label': date_str,
-                'method': 'animate'
-            }
-            for date_str in df['Date_str'].unique()
-        ],
-        'x': 0.1,
-        'xanchor': 'left',
-        'y': -0.2,
-        'yanchor': 'top'
-    }]
+	xaxis_title="Value as Percentage of GDP",
+	yaxis_title="",
+	height=800,  # Adjust the height to make the plot more visible
+	margin=dict(l=40, r=40, t=100, b=40),  # Add margins to make the plot more readable
+	sliders=[{
+		'steps': [
+			{
+				'args': [
+					[date_str],
+					{
+						'frame': {'duration': 300, 'redraw': True},
+						'mode': 'immediate',
+						'transition': {'duration': 300}
+					}
+				],
+				'label': date_str,
+				'method': 'animate'
+			}
+			for date_str in df['Date_str'].unique()
+		],
+		'x': 0.1,
+		'xanchor': 'left',
+		'y': -0.2,
+		'yanchor': 'top'
+	}]
 )
 
-# Initial annotation for the date
-fig.update_layout(
-    annotations=[
-        {
-            'x': 0.5,
-            'y': 1.15,  # Move the date annotation closer to the top of the chart
-            'xref': 'paper',
-            'yref': 'paper',
-            'text': f'Date: {filtered_df["Date_str"].iloc[0]}',
-            'showarrow': False,
-            'font': {
-                'size': 16
-            },
-            'name': 'date_annotation'
-        }
-    ]
-)
+# Add initial annotation for the date
+initial_date_annotation = {
+	'x': 0.5,
+	'y': 1.15,  # Position the date annotation closer to the top of the chart
+	'xref': 'paper',
+	'yref': 'paper',
+	'text': f'Date: {filtered_df["Date_str"].iloc[0]}',
+	'showarrow': False,
+	'font': {
+		'size': 16
+	},
+	'name': 'date_annotation'
+}
+fig.update_layout(annotations=[initial_date_annotation])
+
+# Custom callback to update the date annotation dynamically
+def update_annotations(date_str):
+	return [go.layout.Annotation(
+		x=0.5,
+		y=1.15,
+		xref='paper',
+		yref='paper',
+		text=f'Date: {date_str}',
+		showarrow=False,
+		font=dict(size=16)
+	)]
 
 # Update annotation with each frame
 for frame in fig.frames:
-    date_str = frame.name
-    for annotation in fig['layout']['annotations']:
-        if annotation['name'] == 'date_annotation':
-            annotation['text'] = f'Date: {date_str}'
+	date_str = frame.name
+	frame['layout'].update(annotations=update_annotations(date_str))
 
 # Custom callback to update the date annotation dynamically
 fig.update_layout(
-    updatemenus=[{
-        'type': 'buttons',
-        'showactive': False,
-        'buttons': [
-            {
-                'label': 'Play',
-                'method': 'animate',
-                'args': [None, {
-                    'frame': {'duration': 500, 'redraw': True},
-                    'fromcurrent': True,
-                    'transition': {'duration': 300, 'easing': 'linear'}
-                }]
-            },
-            {
-                'label': 'Pause',
-                'method': 'animate',
-                'args': [[None], {
-                    'frame': {'duration': 0, 'redraw': False},
-                    'mode': 'immediate',
-                    'transition': {'duration': 0}
-                }]
-            }
-        ]
-    }]
+	updatemenus=[{
+		'type': 'buttons',
+		'showactive': False,
+		'buttons': [
+			{
+				'label': 'Play',
+				'method': 'animate',
+				'args': [None, {
+					'frame': {'duration': 500, 'redraw': True},
+					'fromcurrent': True,
+					'transition': {'duration': 300, 'easing': 'linear'}
+				}]
+			},
+			{
+				'label': 'Pause',
+				'method': 'animate',
+				'args': [[None], {
+					'frame': {'duration': 0, 'redraw': False},
+					'mode': 'immediate',
+					'transition': {'duration': 0}
+				}]
+			}
+		]
+	}]
 )
 
 # Display the Plotly figure
