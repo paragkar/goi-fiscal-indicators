4bef90e62f8ced8806714ea82e072d272762d868
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-03 18:45:39 +0530

    Update fiscal-indicators.py

--- fiscal-indicators.py
+++ fiscal-indicators.py
@@ -147,7 +147,7 @@ if selected_metrics:
 		yaxis_title="",
 		width =1200,
 		height=900,  # Adjust the height to make the plot more visible
-		margin=dict(l=0, r=10, t=50, b=40, pad=0),  # Add margins to make the plot more readable and closer to the left
+		margin=dict(l=0, r=10, t=120, b=40, pad=0),  # Add margins to make the plot more readable and closer to the left
 		sliders=[{
 			'steps': [
 				{
